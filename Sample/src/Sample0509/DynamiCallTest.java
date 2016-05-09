d514fdcbf5fad2ea22d63f0b4ba4bdb3dae2b5e3
Moonseunggi <moon44432@naver.com>
2016-05-09 20:48:51 +0900

    0509 static & nested method & override & abstract & polymorphism & inheritance

--- Sample/src/Sample0509/DynamiCallTest.java
+++ Sample/src/Sample0509/DynamiCallTest.java
@@ -9,12 +9,12 @@ package Sample0509;
 	}
 }*/
 
-abstract class AAnimal {                              //추상 클래스
+abstract class AAnimal {                               //추상 클래스
 	abstract void sound();                            //추상 메소드. 몸체가 없는 메소드. Cat과 Dog를 다형성을 이용하기 위해서 사용.
 }
 
 class Dog extends AAnimal {
-	private int d = 11;                               //public이용하면 private이용 가능.
+	private int d = 11;                                //public이용하면 private이용 가능.
 	@Override                                         //오버라이딩이라고 알려주는 것. 없으면 오버라이딩이 아니라 자신의 메소드. 혼동하지 않기 위해 사용.
 	void sound() {                                   //추상 메소드 재정의.
 		System.out.println("멍멍 " + d);
